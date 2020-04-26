a133d21d4e5729c5822a7ede29e02731315bc584
Sean Lim <seanlimdev856@gmail.com>
2020-04-26 12:16:41 +0800

    Add lists layout

--- frontend/src/components/App.svelte
+++ frontend/src/components/App.svelte
@@ -4,17 +4,46 @@
   import { setClient, query } from 'svelte-apollo';
   import { LISTS } from '../queries';
 
+  // Create new Apollo client
   const client = new ApolloClient({ uri: apiURL });
   setClient(client);
 
+  // Fetch data
   const lists = query(client, { query: LISTS });
   $lists.then(console.log);
+
 </script>
 
 <style>
-  h1 {
-    text-align: center;
+  .list-container {
+    height: 100%;
+    display: flex;
+    flex-direction: row;
+    border: solid red 1px;
+    overflow-x: scroll;
+  }
+  .list {
+    padding: 15px;
+    height: 100%;
+    max-width: 33%;
+    min-width: 33%;
+    flex: 1;
+    border-right: solid var(--secondary) 1px;
   }
 </style>
 
-<h1>Hello world!</h1>
+{#await $lists}
+  Loading...
+{:then result}
+  <div class="list-container">
+    {#each result.data.lists as list}
+      <div class="list">
+        <h2>{list.name}</h2>
+      </div>
+    {/each}
+  </div>
+{:catch error}
+  Error: {error}
+{/await}
+
+
